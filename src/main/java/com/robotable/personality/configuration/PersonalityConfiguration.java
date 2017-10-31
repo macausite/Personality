c34c8ac951178eb48bb823c6667d831c805dae4e
Roberto Ambrosio <roberto@ambrosio.ca>
2017-11-01 01:34:26 +0800

    Testing Thymeleaf

--- src/main/java/com/robotable/personality/configuration/PersonalityConfiguration.java
+++ src/main/java/com/robotable/personality/configuration/PersonalityConfiguration.java
@@ -1,5 +1,7 @@
 package com.robotable.personality.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -9,12 +11,17 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.robotable.personality")
 public class PersonalityConfiguration implements WebMvcConfigurer {
-    @Bean
+  /*
+      @Bean
+   
     public ViewResolver viewResolver() {
         InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
         viewResolver.setViewClass(JstlView.class);
@@ -28,5 +35,30 @@ public class PersonalityConfiguration implements WebMvcConfigurer {
         registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
         registry.addResourceHandler("/webjars/**").addResourceLocations("/webjars/");
     }
- 
+ */
+	@Autowired
+	  ApplicationContext applicationContext;
+	  
+	  @Bean
+	  public SpringTemplateEngine templateEngine(){
+	      SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+	      templateEngine.setTemplateResolver(templateResolver());
+	      return templateEngine;
+	  }
+	  
+	  @Bean
+	  public SpringResourceTemplateResolver templateResolver(){
+	      SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+	      templateResolver.setApplicationContext(this.applicationContext);
+	      templateResolver.setPrefix("/WEB-INF/views/");
+	      templateResolver.setSuffix(".html");
+	      return templateResolver;
+	  }
+	  
+	  @Bean
+	  public ViewResolver viewResolver(){
+	      ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+	      viewResolver.setTemplateEngine(templateEngine());
+	      return viewResolver;
+	  }
 }
